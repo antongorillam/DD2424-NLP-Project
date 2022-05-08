427293fff6f4951aa3e517a9984436a73c1c9fca
Anton <whafu@kth.se>
2022-05-08 22:37:29 +0200

    Test run results

--- code/benchmark.py
+++ code/benchmark.py
@@ -19,32 +19,32 @@ if __name__ == '__main__':
     LEARNING_RATE = 0.01
     LABEL_SMOOTHING = 0.8
 
-    # rnn_gen =  rnn.Generator(
-    #     input_string=text, 
-    #     index2char=index2char, 
-    #     char2index=char2index,
-    #     sequence_length=SEQUENCE_LENGTH,
-    #     batch_size=BATCH_SIZE
-    #     )
+    rnn_gen =  rnn.Generator(
+        input_string=text, 
+        index2char=index2char, 
+        char2index=char2index,
+        sequence_length=SEQUENCE_LENGTH,
+        batch_size=BATCH_SIZE
+        )
 
-    # rnn =  rnn.RNN(
-    #     input_size=len(index2char), 
-    #     hidden_size=HIDDEN_SIZE, 
-    #     num_layers=NUM_LAYERS, 
-    #     output_size=len(index2char),
-    # ).to(rnn_gen.device)
+    rnn =  rnn.RNN(
+        input_size=len(index2char), 
+        hidden_size=HIDDEN_SIZE, 
+        num_layers=NUM_LAYERS, 
+        output_size=len(index2char),
+    ).to(rnn_gen.device)
 
-    # rnn_gen.train(
-    #     rnn=rnn,
-    #     num_epchs=NUM_EPOCHS,
-    #     print_every=100,
-    #     lr=LEARNING_RATE,
-    #     temperature=TEMPERATURE,
-    #     label_smoothing=LABEL_SMOOTHING,
-    # )
+    rnn_gen.train(
+        rnn=rnn,
+        num_epchs=NUM_EPOCHS,
+        print_every=100,
+        lr=LEARNING_RATE,
+        temperature=TEMPERATURE,
+        label_smoothing=LABEL_SMOOTHING,
+    )
     
-    # rnn_df = pd.DataFrame(rnn_gen.history)
-    # rnn_df.to_csv(f'{DIR}/lstm.csv', index=False) # TODO: add model configs to filename
+    rnn_df = pd.DataFrame(rnn_gen.history)
+    rnn_df.to_csv(f'{DIR}/rnn.csv', index=False) # TODO: add model configs to filename
 
     lstm_gen = lstm.Generator(
         input_string=text, 
