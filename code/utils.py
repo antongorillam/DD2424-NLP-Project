a815ec4d730754a6d9d41dda77674f5a12f1df69
Kunal Bhatnagar <kubha99@kth.se>
2022-05-16 00:35:29 +0200

    this should work

--- code/utils.py
+++ code/utils.py
@@ -16,22 +16,22 @@ def read_data():
                 dict mapping each index to characters
     """
     train_dir = [
-        "data/train/The_Sun_Also_Rises.txt",
-        "data/train/A_Movable_Feast.txt",
-        "data/train/Death_In_The_Afternoon.txt",
-        "data/train/For_Whom_The_Bell_Tolls.txt",
-        "data/train/Men_Without_Women.txt",
-        "data/train/Three_Stories_And_Ten_poems.txt",
+        "../data/train/The_Sun_Also_Rises.txt",
+        "../data/train/A_Movable_Feast.txt",
+        "../data/train/Death_In_The_Afternoon.txt",
+        "../data/train/For_Whom_The_Bell_Tolls.txt",
+        "../data/train/Men_Without_Women.txt",
+        "../data/train/Three_Stories_And_Ten_poems.txt",
         ]
 
 
     test_dir = [
-        "data/test/Across_The_River_And_Into_The_Trees.txt",
-        "data/test/A_Farewell_To_Arms.txt",
-        "data/test/Grenn_Hills_of_Africa.txt",
-        "data/test/In_Our_Time.txt",
-        "data/test/Old_Man_And_The_Sea.txt",
-        "data/test/The_Torrents_Of_Spring.txt",
+        "../data/test/Across_The_River_And_Into_The_Trees.txt",
+        "../data/test/A_Farewell_To_Arms.txt",
+        "../data/test/Grenn_Hills_of_Africa.txt",
+        "../data/test/In_Our_Time.txt",
+        "../data/test/Old_Man_And_The_Sea.txt",
+        "../data/test/The_Torrents_Of_Spring.txt",
     ]
 
     train_text = ""
