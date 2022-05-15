e187bacc68117ffe1a897c1458c9a4ff9364d165
Kunal Bhatnagar <kubha99@kth.se>
2022-05-16 01:20:50 +0200

    updated files

--- code/load.py
+++ code/load.py
@@ -8,7 +8,7 @@ import pandas as pd
 class loadModel:
 
     def __init__(self, modelDir="../results/rnn_vs_lstm/lstm_hidden100_epoch100000_lr0.01_nlayer2.pth", h_size=100, n_layer=2):
-        device = torch.device("cuda" if not torch.cuda.is_available() else "cpu")
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.lstm_gen = load_model(
             dir=modelDir,
             hidden_size=h_size,
