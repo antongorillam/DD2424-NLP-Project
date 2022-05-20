eb9c68e1c27980e7873333706f75ad1e336a59bd
Anton <whafu@kth.se>
2022-05-21 00:33:49 +0200

    added lineplots

--- code/plot.py
+++ code/plot.py
@@ -190,97 +190,104 @@ def plot_string(this_temp):
     plt.xlabel("Hidden Size")
     bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_bert_types_temp_{this_temp}.png')
 
-def plot_temperature_metrics(this_temp):
+def lineplot_metrics(metric="temperature"):
     import matplotlib.pyplot as plt
     import pandas as pd
 
-    temp_check = pd.read_csv("../results/score_check/metric_check.csv")
-    temp_check = temp_check[temp_check["temperature"]==this_temp]
-    SAVE_DIR = "../results/score_check/plots"
+    df = pd.read_csv("../results/score_check/metric_check.csv")
+    SAVE_DIR = f"../results/score_check/plots/{metric}_lineplot"
 
     sns.set_style("whitegrid")
     """
     Get spelling percentage
     """
     plt.figure()
-    title_string = f"Spelling pecetange (the higher the better), temperature: {this_temp}"
-    bar_plot = sns.barplot(x=temp_check.hidden_size, y=temp_check.spelling_percentage, palette="crest").set_title(title_string)
+    title_string = f"Spelling pecetange (the higher the better)"
+    lineplot = sns.lineplot(x=metric, y="spelling_percentage", data=df, hue=None, palette="crest", legend=True).set_title(title_string)
     plt.ylabel("Spelling Percentage")
-    plt.xlabel("Hidden Size")
-    bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_plot_spelling_pecetange_temp_{this_temp}.png')
+    plt.xlabel(metric)
+    lineplot.get_figure().savefig(f'{SAVE_DIR}/lineplot_spelling_pecetange_{metric}.png')
 
     plt.figure()
-    title_string = f"Perplexity score (the lower the better), temperature: {this_temp}"
-    bar_plot = sns.barplot(x=temp_check.hidden_size, y=temp_check.perplexity, palette="crest").set_title(title_string)
+    title_string = f"Perplexity score (the lower the better)"
+    lineplot = sns.lineplot(x=df[metric], y=df.perplexity, hue=None, palette="crest").set_title(title_string)
     plt.ylabel("Perplexity Score")
-    plt.xlabel("Hidden Size")
-    bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_plot_perplexity_temp_{this_temp}.png')
+    plt.xlabel(metric)
+    lineplot.get_figure().savefig(f'{SAVE_DIR}/lineplot_perplexity_{metric}.png')
 
     plt.figure()
-    title_string = f"TTR score (the higher the better), temperature: {this_temp}"
-    bar_plot = sns.barplot(x=temp_check.hidden_size, y=temp_check.TTR, palette="crest").set_title(title_string)
+    title_string = f"TTR score (the higher the better)"
+    lineplot = sns.lineplot(x=df[metric], y=df.TTR, hue=None, palette="crest").set_title(title_string)
     plt.ylabel("TTR Score")
-    plt.xlabel("Hidden Size")
-    bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_plot_TTR_temp_{this_temp}.png')
+    plt.xlabel(metric)
+    lineplot.get_figure().savefig(f'{SAVE_DIR}/lineplot_TTR_{metric}.png')
 
     plt.figure()
-    title_string = f"Bartscore (the higher the better), temperature: {this_temp}"
-    bar_plot = sns.barplot(x=temp_check.hidden_size, y=temp_check.bartscore, palette="crest").set_title(title_string)
+    title_string = f"Bartscore (the higher the better)"
+    lineplot = sns.lineplot(x=df[metric], y=df.bartscore, hue=None, palette="crest").set_title(title_string)
     plt.ylabel("Bartscore")
-    plt.xlabel("Hidden Size")
-    bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_bartscore_temp_{this_temp}.png')
+    plt.xlabel(metric)
+    lineplot.get_figure().savefig(f'{SAVE_DIR}/lineplot_bartscore_{metric}.png')
 
-    bleu1 = temp_check[["temperature", "hidden_size", "bleu1"]] 
+    bleu1 = df[[metric, "hidden_size", "bleu1"]] 
     bleu1["bleu type"] = "bleu1"
     bleu1 = bleu1.rename(columns={"bleu1":"bleu"})
 
-    bleu2 = temp_check[["temperature", "hidden_size", "bleu2"]] 
+    bleu2 = df[[metric, "hidden_size", "bleu2"]] 
     bleu2["bleu type"] = "bleu2"
     bleu2 = bleu2.rename(columns={"bleu2":"bleu"})
 
-    bleu3 = temp_check[["temperature", "hidden_size", "bleu3"]] 
+    bleu3 = df[[metric, "hidden_size", "bleu3"]] 
     bleu3["bleu type"] = "bleu3"
     bleu3 = bleu3.rename(columns={"bleu3":"bleu"})
 
-    bleu4 = temp_check[["temperature", "hidden_size", "bleu4"]] 
+    bleu4 = df[[metric, "hidden_size", "bleu4"]] 
     bleu4["bleu type"] = "bleu4"
     bleu4 = bleu4.rename(columns={"bleu4":"bleu"})
-
     bleu_df = pd.concat([bleu1, bleu2, bleu3, bleu4])
 
     plt.figure()
-    title_string = f"BLEU Score, temperature (the higher the better): {this_temp}"
-    bar_plot = sns.barplot(x="hidden_size", y="bleu", data=bleu_df, hue="bleu type", palette="crest").set_title(title_string)
+    title_string = f"BLEU Score, {metric} (the higher the better)"
+    line_bleu1 = sns.lineplot(x=metric, y="bleu", data=bleu1, hue=None, palette="crest", legend=True).set_title(title_string)
+    line_bleu2 = sns.lineplot(x=metric, y="bleu", data=bleu2, hue=None, palette="crest", legend=True).set_title(title_string)
+    line_bleu3 = sns.lineplot(x=metric, y="bleu", data=bleu3, hue=None, palette="crest", legend=True).set_title(title_string)
+    line_bleu4 = sns.lineplot(x=metric, y="bleu", data=bleu4, hue=None, palette="crest", legend=True).set_title(title_string)
     plt.ylabel("BLEU Score")
-    plt.xlabel("Hidden Size")
-    plt.legend(bbox_to_anchor=(0.93, 1), loc='upper left', borderaxespad=0)
-    bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_bleu_temp_{this_temp}.png')
+    plt.xlabel(metric)
+    plt.legend(["bleu1", '_nolegend_', "bleu2", '_nolegend_', "bleu3", '_nolegend_', "bleu4", '_nolegend_',])
+    plt.savefig(f'{SAVE_DIR}/lineplot_bleu_{metric}.png')
+    
 
-    bertscore = temp_check[["temperature", "hidden_size", "bertscore"]] 
+    plt.figure()
+    bertscore = df[["temperature", "hidden_size", "bertscore"]] 
     bertscore["bertscore type"] = "bertscore"
     bertscore = bertscore.rename(columns={"bertscore":"bert"})
 
-    bertscore_precision = temp_check[["temperature", "hidden_size", "bertscore_precision"]] 
+    bertscore_precision = df[["temperature", "hidden_size", "bertscore_precision"]] 
     bertscore_precision["bertscore type"] = "bertscore_precision"
     bertscore_precision = bertscore_precision.rename(columns={"bertscore_precision":"bert"})
 
-    bertscore_recall = temp_check[["temperature", "hidden_size", "bertscore_recall"]] 
+    bertscore_recall = df[["temperature", "hidden_size", "bertscore_recall"]] 
     bertscore_recall["bertscore type"] = "bertscore_recall"
     bertscore_recall = bertscore_recall.rename(columns={"bertscore_recall":"bert"})
 
-    bertscore_f1 = temp_check[["temperature", "hidden_size", "bertscore_f1"]] 
+    bertscore_f1 = df[["temperature", "hidden_size", "bertscore_f1"]] 
     bertscore_f1["bertscore type"] = "bertscore_f1"
     bertscore_f1 = bertscore_f1.rename(columns={"bertscore_f1":"bert"})
 
-    bertscore = pd.concat([bertscore, bertscore_precision, bertscore_recall, bertscore_f1])
-
+        
+    # bertscore = pd.concat([bertscore, bertscore_precision, bertscore_recall, bertscore_f1])
     plt.figure()
-    title_string = f"BertScore, temperature (the higher the better): {this_temp}"
-    bar_plot = sns.barplot(x="hidden_size", y="bert", data=bertscore, hue="bertscore type", palette="crest").set_title(title_string)
-    plt.legend(bbox_to_anchor=(0.75, 0.3), loc='upper left', borderaxespad=0)
+    title_string = f"BertScore (the higher the better)"
+    sns.lineplot(x=metric, y="bert", data=bertscore, hue=None, palette="crest").set_title(title_string)
+    # sns.lineplot(x=metric, y="bert", data=bertscore_precision, hue=None, palette="crest").set_title(title_string)
+    # sns.lineplot(x=metric, y="bert", data=bertscore_recall, hue=None, palette="crest").set_title(title_string)
+    # sns.lineplot(x=metric, y="bert", data=bertscore_f1, hue=None, palette="crest").set_title(title_string)
+    
     plt.ylabel("Bert Score")
-    plt.xlabel("Hidden Size")
-    bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_bert_types_temp_{this_temp}.png')
+    plt.xlabel(metric)
+    plt.savefig(f"{SAVE_DIR}/lineplot_bert_{metric}.png")
+    # lineplot.get_figure().savefig(f'{SAVE_DIR}/lineplot_bert_types_{this_temp}.png')
 
 if __name__ == '__main__':
-    plot_metrics(0.7)
+    lineplot_metrics()
--- code/runs.py
+++ code/runs.py
@@ -111,117 +111,95 @@ class Run:
 #     run.run_lstm(hyper_params=h, save=True, print_every=100)  
 
 # if __name__=='__main__':
-    
-#     hidden_25 = pd.read_csv("../results/anton_test/learning_rate_0005/lstm_hidden25_epoch10000_lr0.005_nlayer2.csv")
-#     hidden_50 = pd.read_csv("../results/anton_test/learning_rate_0005/lstm_hidden50_epoch10000_lr0.005_nlayer2.csv")
-#     hidden_250 = pd.read_csv("../results/anton_test/learning_rate_0005/lstm_hidden250_epoch10000_lr0.005_nlayer2.csv")
-#     hidden_500 = pd.read_csv("../results/anton_test/learning_rate_0005/lstm_hidden500_epoch10000_lr0.005_nlayer2.csv")
+#     hidden25_temp01 = pd.read_csv("hidden25_temp0.1_check.csv")
+#     hidden50_temp01 = pd.read_csv("hidden50_temp0.1_check.csv")
+#     hidden250_temp01 = pd.read_csv("hidden250_temp0.1_check.csv")
+#     hidden500_temp01 = pd.read_csv("hidden500_temp0.1_check.csv")
 
-#     hidden_25["Hidden Size"] = 25
-#     hidden_50["Hidden Size"] = 50        
-#     hidden_250["Hidden Size"] = 250      
-#     hidden_500["Hidden Size"] = 500      
+#     hidden25_temp03 = pd.read_csv("hidden25_temp0.3_check.csv")
+#     hidden50_temp03 = pd.read_csv("hidden50_temp0.3_check.csv")
+#     hidden250_temp03 = pd.read_csv("hidden250_temp0.3_check.csv")
+#     hidden500_temp03 = pd.read_csv("hidden500_temp0.3_check.csv")
 
-#     df = pd.concat([hidden_25, hidden_50, hidden_250, hidden_500], ignore_index=True, axis=0)
+#     hidden25_temp05 = pd.read_csv("hidden25_temp0.5_check.csv")
+#     hidden50_temp05 = pd.read_csv("hidden50_temp0.5_check.csv")
+#     hidden250_temp05 = pd.read_csv("hidden250_temp0.5_check.csv")
+#     hidden500_temp05 = pd.read_csv("hidden500_temp0.5_check.csv")
 
-#     sns.set_style("whitegrid")
+#     hidden25_temp07 = pd.read_csv("hidden25_temp0.7_check.csv")
+#     hidden50_temp07 = pd.read_csv("hidden50_temp0.7_check.csv")
+#     hidden250_temp07 = pd.read_csv("hidden250_temp0.7_check.csv")
+#     hidden500_temp07 = pd.read_csv("hidden500_temp0.7_check.csv")
 
-#     title_string = f"Loss vs iterations, lr:{0.005}"
-#     line_plot = sns.lineplot(data=df, x="iterations", y="loss", hue="Hidden Size", palette=["r","b","g","orange"]).set_title(title_string)
-#     line_plot.get_figure().savefig(f'combined_line_plot_lr{0.005}_nlayer{2}.png')
+#     hidden25_temp09 = pd.read_csv("hidden25_temp0.9_check.csv")
+#     hidden50_temp09 = pd.read_csv("hidden50_temp0.9_check.csv")
+#     hidden250_temp09 = pd.read_csv("hidden250_temp0.9_check.csv")
+#     hidden500_temp09 = pd.read_csv("hidden500_temp0.9_check.csv")
 
-#     plt.figure()
-#     title_string = f"Final loss, lr:{0.005}"
-#     hidden_size = [25, 50, 250, 500]
-#     loss = [hidden_25["loss"][100], hidden_50["loss"][100], hidden_250["loss"][100], hidden_500["loss"][100]]
-#     bar_plot = sns.barplot(x=hidden_size, y=loss, palette=["r","b","g","orange"]).set_title(title_string)
-#     plt.ylabel("loss")
-#     plt.xlabel("Hidden Size")
-#     bar_plot.get_figure().savefig(f'combined_bar_plot_lr{0.005}_nlayer{2}.png')
+#     hidden25_temp11 = pd.read_csv("hidden25_temp1.1_check.csv")
+#     hidden50_temp11 = pd.read_csv("hidden50_temp1.1_check.csv")
+#     hidden250_temp11 = pd.read_csv("hidden250_temp1.1_check.csv")
+#     hidden500_temp11 = pd.read_csv("hidden500_temp1.1_check.csv")
 
+#     hidden25_temp13 = pd.read_csv("hidden25_temp1.3_check.csv")
+#     hidden50_temp13 = pd.read_csv("hidden50_temp1.3_check.csv")
+#     hidden250_temp13 = pd.read_csv("hidden250_temp1.3_check.csv")
+#     hidden500_temp13 = pd.read_csv("hidden500_temp1.3_check.csv")
 
-# if __name__=='__main__':
-#     import pandas as pd
-#     import matplotlib.pyplot as plt
+#     hidden25_temp15 = pd.read_csv("hidden25_temp1.5_check.csv")
+#     hidden50_temp15 = pd.read_csv("hidden50_temp1.5_check.csv")
+#     hidden250_temp15 = pd.read_csv("hidden250_temp1.5_check.csv")
+#     hidden500_temp15 = pd.read_csv("hidden500_temp1.5_check.csv")
 
-#     temp_check = pd.read_csv("../results/score_check/hidden_check_temp_09.csv")
-#     SAVE_DIR = "../results/score_check/plots"
-#     sns.set_style("whitegrid")
-#     """
-#     Get spelling percentage
-#     """
-#     plt.figure()
-#     title_string = f"Spelling pecetange (the higher the better), temperature: {temp_check.temperature[0]}"
-#     bar_plot = sns.barplot(x=temp_check.hidden_size, y=temp_check.spelling_percentage, palette=["r","b","g","orange"]).set_title(title_string)
-#     plt.ylabel("Spelling Percentage")
-#     plt.xlabel("Hidden Size")
-#     bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_plot_spelling_pecetange_temp_{temp_check.temperature[0]}.png')
+#     hidden25_temp17 = pd.read_csv("hidden25_temp1.7_check.csv")
+#     hidden50_temp17 = pd.read_csv("hidden50_temp1.7_check.csv")
+#     hidden250_temp17 = pd.read_csv("hidden250_temp1.7_check.csv")
+#     hidden500_temp17 = pd.read_csv("hidden500_temp1.7_check.csv")
 
-#     plt.figure()
-#     title_string = f"Perplexity score (the lower the better), temperature: {temp_check.temperature[0]}"
-#     bar_plot = sns.barplot(x=temp_check.hidden_size, y=temp_check.perplexity, palette=["r","b","g","orange"]).set_title(title_string)
-#     plt.ylabel("Perplexity Score")
-#     plt.xlabel("Hidden Size")
-#     bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_plot_perplexity_temp_{temp_check.temperature[0]}.png')
+#     hidden25_temp19 = pd.read_csv("hidden25_temp1.9_check.csv")
+#     hidden50_temp19 = pd.read_csv("hidden50_temp1.9_check.csv")
+#     hidden250_temp19 = pd.read_csv("hidden250_temp1.9_check.csv")
+#     hidden500_temp19 = pd.read_csv("hidden500_temp1.9_check.csv")
 
-
-#     plt.figure()
-#     title_string = f"Bartscore (the higher the better), temperature: {temp_check.temperature[0]}"
-#     bar_plot = sns.barplot(x=temp_check.hidden_size, y=temp_check.bartscore, palette=["r","b","g","orange"]).set_title(title_string)
-#     plt.ylabel("Bartscore")
-#     plt.xlabel("Hidden Size")
-#     bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_bartscore_temp_{temp_check.temperature[0]}.png')
-
-#     bleu1 = temp_check[["temperature", "hidden_size", "bleu1"]] 
-#     bleu1["bleu type"] = "bleu1"
-#     bleu1 = bleu1.rename(columns={"bleu1":"bleu"})
-
-#     bleu2 = temp_check[["temperature", "hidden_size", "bleu2"]] 
-#     bleu2["bleu type"] = "bleu2"
-#     bleu2 = bleu2.rename(columns={"bleu2":"bleu"})
-
-#     bleu3 = temp_check[["temperature", "hidden_size", "bleu3"]] 
-#     bleu3["bleu type"] = "bleu3"
-#     bleu3 = bleu3.rename(columns={"bleu3":"bleu"})
-
-#     bleu4 = temp_check[["temperature", "hidden_size", "bleu4"]] 
-#     bleu4["bleu type"] = "bleu4"
-#     bleu4 = bleu4.rename(columns={"bleu4":"bleu"})
-
-#     bleu_df = pd.concat([bleu1, bleu2, bleu3, bleu4])
-
-#     plt.figure()
-#     title_string = f"BLEU Score, temperature (the higher the better): {temp_check.temperature[0]}"
-#     bar_plot = sns.barplot(x="hidden_size", y="bleu", data=bleu_df, hue="bleu type", palette="crest").set_title(title_string)
-#     plt.ylabel("BLEU Score")
-#     plt.xlabel("Hidden Size")
-#     plt.legend(bbox_to_anchor=(0.93, 1), loc='upper left', borderaxespad=0)
-#     bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_bleu_temp_{temp_check.temperature[0]}.png')
-
-#     bertscore = temp_check[["temperature", "hidden_size", "bertscore"]] 
-#     bertscore["bertscore type"] = "bertscore"
-#     bertscore = bertscore.rename(columns={"bertscore":"bert"})
-
-#     bertscore_precision = temp_check[["temperature", "hidden_size", "bertscore_precision"]] 
-#     bertscore_precision["bertscore type"] = "bertscore_precision"
-#     bertscore_precision = bertscore_precision.rename(columns={"bertscore_precision":"bert"})
-
-#     bertscore_recall = temp_check[["temperature", "hidden_size", "bertscore_recall"]] 
-#     bertscore_recall["bertscore type"] = "bertscore_recall"
-#     bertscore_recall = bertscore_recall.rename(columns={"bertscore_recall":"bert"})
-
-#     bertscore_f1 = temp_check[["temperature", "hidden_size", "bertscore_f1"]] 
-#     bertscore_f1["bertscore type"] = "bertscore_f1"
-#     bertscore_f1 = bertscore_f1.rename(columns={"bertscore_f1":"bert"})
-
-#     bertscore = pd.concat([bertscore, bertscore_precision, bertscore_recall, bertscore_f1])
-
-#     plt.figure()
-#     title_string = f"BertScore, temperature (the higher the better): {temp_check.temperature[0]}"
-#     bar_plot = sns.barplot(x="hidden_size", y="bert", data=bertscore, hue="bertscore type", palette="crest").set_title(title_string)
-#     plt.legend(bbox_to_anchor=(0.75, 0.3), loc='upper left', borderaxespad=0)
-#     plt.ylabel("Bert Score")
-#     plt.xlabel("Hidden Size")
-#     bar_plot.get_figure().savefig(f'{SAVE_DIR}/bar_bert_types_temp_{temp_check.temperature[0]}.png')
-    
-
+#     df = pd.concat([
+#         hidden25_temp01,
+#         hidden50_temp01,
+#         hidden250_temp01,
+#         hidden500_temp01,
+#         hidden25_temp03,
+#         hidden50_temp03,
+#         hidden250_temp03,
+#         hidden500_temp03,
+#         hidden25_temp05,
+#         hidden50_temp05,
+#         hidden250_temp05,
+#         hidden500_temp05,
+#         hidden25_temp07,
+#         hidden50_temp07,
+#         hidden250_temp07,
+#         hidden500_temp07,
+#         hidden25_temp09,
+#         hidden50_temp09,
+#         hidden250_temp09,
+#         hidden500_temp09,
+#         hidden25_temp11,
+#         hidden50_temp11,
+#         hidden250_temp11,
+#         hidden500_temp11,
+#         hidden25_temp13,
+#         hidden50_temp13,
+#         hidden250_temp13,
+#         hidden500_temp13,
+#         hidden25_temp15,
+#         hidden50_temp15,
+#         hidden250_temp15,
+#         hidden500_temp15,
+#         hidden25_temp17,
+#         hidden50_temp17,
+#         hidden250_temp17,
+#         hidden500_temp17,
+#         hidden25_temp19,
+#         hidden50_temp19,
+#         hidden250_temp19,
+#         hidden500_temp19,
+#     ])
